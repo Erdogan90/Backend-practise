208e08e222ce4e6f278f36fd1f1cdce5314f6ded
Erdogan90 <94233632+Erdogan90@users.noreply.github.com>
2022-02-11 12:10:43 +0000

    env

---
+++ config.js
@@ -0,0 +1,9 @@
+const database = {
+user : process.env.PGUSER,
+host : process.env.PGHOST,
+database : process.env.PGDATABASE,
+password : process.env.PGPASSWORD,
+port: process.env.PGPORT,
+}
+
+export default database
---
+++ db/index.js
@@ -0,0 +1,12 @@
+import pg from "pg"
+import database from "../config"
+
+const pool = new pg.Pool({
+    user: database.user,
+    host: database.host,
+    database: database.database,
+    password: database.password,
+    port: database.port,
+    ssl: { rejectUnauthorized: false }
+})
+export default pool
